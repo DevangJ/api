4d2db83d323a0c84e7887e562937688e6d6f731e
devangj <devang.j1998@gmail.com>
2019-07-25 17:23:32 +0530

    25-7-19

--- application.py
+++ application.py
@@ -31,7 +31,6 @@ def invalid_params(error=None):
 @app.route('/sql', methods=["POST"])
 def sql():
   if request.method == "POST":
-    print(request.form["datatype"])
     return sql_response(request.form["datatype"], request.form["input"])
   else:
     return not_allowed()
@@ -40,8 +39,15 @@ def sql():
 @app.route('/url', methods=["POST"])
 def url():
   if request.method == "POST":
-    body = request.json
-    return url_response(body["datatype"], body["input"])
+    return url_response(request.form["input"])
+  else:
+    return not_allowed()
+
+
+@app.route('/xss', methods=["POST"])
+def xss():
+  if request.method == "POST":
+    return xss_response(request.form["type"], request.form["input"])
   else:
     return not_allowed()
 
@@ -72,8 +78,32 @@ def sql_response(datatype, input):
   return resp
 
 
+def url_response(input):
+  if input:
+    message = url_escape(input)
+  else:
+    return invalid_params()
+  resp = jsonify(message)
+  resp.status_code = 200
+  return resp
 
 
+def xss_response(type_, input):
+  if type_ == "html":
+    html_escape(input)
+  elif type_ == "attribute":
+    attr_escape(input)
+  elif type_ == "javascript":
+    js_escape(input)
+  elif type_ == "css":
+    css_escape(input)
+  elif type_ == "url":
+    xss_url_escape(input)
+  else:
+    return invalid_params()
+  resp = jsonify(message)
+  resp.status_code = 200
+  return resp
 
 
 if __name__ == '__main__':
--- custom_functions.py
+++ custom_functions.py
@@ -5,7 +5,37 @@ html_escape_table = {
     '"': "&quot;",
     "'": "&#x27;",
     "/": "&#x2F;"
-    }
+}
+
+url_escape_table = {
+	"!"	:	"%21",
+	"#"	:	"%23",
+	"$"	:	"%24",
+	"%"	:	"%25",
+	"&"	:	"%26",
+	"'"	:	"%27",
+	"("	:	"%28",
+	")"	:	"%29",
+	"*"	:	"%2A",
+	"+"	:	"%2B",
+	","	:	"%2C",
+	"/"	:	"%2F",
+	":"	:	"%3A",
+	";"	:	"%3B",
+	"="	:	"%3D",
+	"?"	:	"%3F",
+	"@"	:	"%40",
+	"["	:	"%5B",
+	"]"	:	"%5D"
+}
+
+
+def url_escape(char):
+    if url_escape_table[char]:
+        return url_escape_table[char]
+    else:
+        return char
+
 
 def html_escape(text):
     return "".join(html_escape_table.get(c,c) for c in text)
